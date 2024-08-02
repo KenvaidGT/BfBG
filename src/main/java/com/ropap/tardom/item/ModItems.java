782a050330fd53ddc8420fbb6ac51c3f19e82f54
KenvaidGT <KenvaidGT@gmail.com>
2024-08-02 17:37:20 +0300

    Sorted source paths

--- src/main/java/com/ropap/tardom/item/ModItems.java
+++ src/main/java/com/ropap/tardom/item/ModItems.java
@@ -15,36 +15,43 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, TCI.MOD_ID);
 
     //
-
-    public static final RegistryObject<Item> KHORNE_AX_BLADE = ITEMS.register("khorne_ax_blade",
+    // Craft parts:
+    public static final RegistryObject<Item> KHORNE_AX_BLADE = ITEMS.register("craft_parts/khorne_ax_blade",
             () -> new Item(new Item.Properties().tab(ModItemGroup.BfBG).stacksTo(1)));
 
-    public static final RegistryObject<Item> KHORNE_AX_HANDLE = ITEMS.register("khorne_ax_handle",
+    public static final RegistryObject<Item> KHORNE_AX_HANDLE = ITEMS.register("craft_parts/khorne_ax_handle",
             () -> new Item(new Item.Properties().tab(ModItemGroup.BfBG).stacksTo(1)));
 
-    public static final RegistryObject<Item> KHORNE_AX_SPIKE = ITEMS.register("khorne_ax_spike",
+    public static final RegistryObject<Item> KHORNE_AX_SPIKE = ITEMS.register("craft_parts/khorne_ax_spike",
             () -> new Item(new Item.Properties().tab(ModItemGroup.BfBG).stacksTo(1)));
 
-    public static final RegistryObject<Item> KHORNE_AX_POMMEL = ITEMS.register("khorne_ax_pommel",
+    public static final RegistryObject<Item> KHORNE_AX_POMMEL = ITEMS.register("craft_parts/khorne_ax_pommel",
             () -> new Item(new Item.Properties().tab(ModItemGroup.BfBG).stacksTo(1)));
 
-    public static final RegistryObject<Item> KHORNE_AX_HEAD = ITEMS.register("khorne_ax_head",
+    public static final RegistryObject<Item> KHORNE_AX_HEAD = ITEMS.register("craft_parts/khorne_ax_head",
             () -> new Item(new Item.Properties().tab(ModItemGroup.BfBG).stacksTo(1)));
 
-    public static final RegistryObject<AxeItem> KHORNE_AXE = ITEMS.register("khorne_axe",
+    //Materials:
+
+    public static final RegistryObject<Item> BLOOD_INGOT = ITEMS.register("materials/blood_ingot",
+            () -> new Item(new Item.Properties().tab(ModItemGroup.BfBG).stacksTo(64)));
+
+    public static final RegistryObject<Item> BLACK_STEEL_INGOT = ITEMS.register("materials/black_steel_ingot",
+            () -> new Item(new Item.Properties().tab(ModItemGroup.BfBG).stacksTo(64)));
+
+    public static final RegistryObject<Item> BRASS_INGOT = ITEMS.register("materials/brass_ingot",
+            () -> new Item(new Item.Properties().tab(ModItemGroup.BfBG).stacksTo(64)));
+
+    public static final RegistryObject<Item> COPPER_INGOT = ITEMS.register("materials/copper_ingot",
+            () -> new Item(new Item.Properties().tab(ModItemGroup.BfBG).stacksTo(64)));
+
+    // Instruments:
+
+    public static final RegistryObject<AxeItem> KHORNE_AXE = ITEMS.register("instruments/khorne_axe",
             () -> new AxeItem(ModItemTier.AXE, 18, -3.2f, new Item.Properties().tab(ModItemGroup.BfBG)));
 
-    public static final RegistryObject<Item> KHORNE_SHIELD = ITEMS.register("khorne_shield",
-            () -> new ShieldItem(new Item.Properties().tab(ModItemGroup.BfBG).durability(336)));
-
-    public static final RegistryObject<Item> BLOOD_INGOT = ITEMS.register("blood_ingot",
-            () -> new Item(new Item.Properties().tab(ModItemGroup.BfBG).stacksTo(64)));
-
-    public static final RegistryObject<Item> BLACK_STEEL_INGOT = ITEMS.register("black_steel_ingot",
-            () -> new Item(new Item.Properties().tab(ModItemGroup.BfBG).stacksTo(64)));
-
-    public static final RegistryObject<Item> BRASS_INGOT = ITEMS.register("brass_ingot",
-            () -> new Item(new Item.Properties().tab(ModItemGroup.BfBG).stacksTo(64)));
+    public static final RegistryObject<Item> KHORNE_SHIELD = ITEMS.register("instruments/khorne_shield",
+            () -> new ShieldItem(new Item.Properties().tab(ModItemGroup.BfBG).durability(888)));
 
 
     //
